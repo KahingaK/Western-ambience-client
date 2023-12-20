d15df1709dad389f42f890a318295ce527342faa
IvanKahinga <ikahinga@gmail.com>
2023-12-20 11:47:28 +0300

    menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import menu from "../assets/menu.jpg"
 
-function Menu() {
+function Menu({onClose}) {
   return (
-    <div><div>plater</div>
-    <div>items</div></div>
+    <div className="popup">
+    <div className="popup-content lg:max-h-screen  lg:w-auto w-[95%] max-h-screen overflow-auto">
+      <div className="h-full relative flex justify-center items-center">
+        <span className="close text-3xl pr-4" onClick={onClose}>
+          &times;
+        </span>
+        <div className="h-full">
+          <img
+            src={menu}
+            alt="ambience hotel menu"
+            className="w-full h-full"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
   )
 }
 
--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import food from "../assets/img/food.png";
+import Menu from './Menu';
 
 function Restaurant() {
+const [isShowPopup, setShowPopup] = useState(false)
+
+function handleShowMenu() {
+  setShowPopup(true)
+  
+}
+
+function handleCloseMenu() {
+  setShowPopup(false)
+}
+  
+  
   return (
     <section className="py-32">
       <div className="container mx-auto lg:px-0">
@@ -15,8 +28,14 @@ function Restaurant() {
           <div className='flex-1 text-center lg:text-left  justify-center items-center  mb-14 '>
             <h2 className="font-primary text-4xl mb-4">Visit our Kitchen</h2>
             <p className='font-secondary mb-8 max-w-[620px]'>Come indulge your senses at Western Ambience Hotel. We are proud to provide unique and exceptional food and drink options from our talented kitchen and bar staff, who work hard all year to come up with inspired seasonal menus. Join us for an unforgettable culinary experience. Browse our menu and contact us in advance for reservations or with any questions.</p>
-            <button className='btn btn-primary uppercase mx-auto lg:mx-0 py-4 px-12'>See Menu</button>
+            <button 
+            onClick={handleShowMenu}
+            className='btn btn-primary uppercase mx-auto lg:mx-0 py-4 px-12'>See Menu</button>
           </div>
+          {isShowPopup && <Menu
+          onClose={handleCloseMenu}
+          />}
+          
           <div className=" z-10">
             <img src={food} alt="" />
           </div>
